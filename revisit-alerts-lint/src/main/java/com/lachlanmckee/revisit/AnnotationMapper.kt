5c41bc605485f70421dedf6d17dec896d79690bd
Lachlan McKee <lachlandevelopment@gmail.com>
2021-08-30 21:15:16 +0100

    Pass issue into model mapper for future detectors

--- revisit-alerts-lint/src/main/java/com/lachlanmckee/revisit/AnnotationMapper.kt
+++ revisit-alerts-lint/src/main/java/com/lachlanmckee/revisit/AnnotationMapper.kt
@@ -1,12 +1,18 @@
 package com.lachlanmckee.revisit
 
+import com.android.tools.lint.detector.api.Issue
 import com.android.tools.lint.detector.api.JavaContext
 import org.jetbrains.uast.UAnnotation
 
 class AnnotationMapper {
-  fun <T> mapToModel(context: JavaContext, annotation: UAnnotation, func: ModelMapper.() -> T): T? =
+  fun <T> mapToModel(
+    context: JavaContext,
+    annotation: UAnnotation,
+    issue: Issue,
+    func: ModelMapper.() -> T
+  ): T? =
     try {
-      func(ModelMapper(context, annotation, annotation.attributeValues))
+      func(ModelMapper(context, annotation, issue, annotation.attributeValues))
     } catch (e: Exception) {
       null
     }
